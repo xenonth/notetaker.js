962136939b0f35be9dd9afdee551129af07406c9
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-03 09:35:59 +1030

    can retrieve index.html on localhost:3001

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+// Application Dependencies
+const express = require("express");
+const path = require("path");
+
 // variables for port and routing
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Accessing the database
+
+//ROUTES
 
 //route for GET index.html (`/`)
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+
 
 //route for GET blah/notes.html (`/notes.html)
 
@@ -9,3 +28,9 @@
 //POST api/notes
 
 //ability to delete said notes
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
