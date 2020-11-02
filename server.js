22a74ac8cfb820ef9609d84115645672e5429da7
Timothy Forbes <timothy.forbes259@gmail.com>
2020-11-03 09:24:35 +1030

    startup

---
+++ server.js
@@ -0,0 +1,11 @@
+// variables for port and routing
+
+//route for GET index.html (`/`)
+
+//route for GET blah/notes.html (`/notes.html)
+
+//route for GET api/notes
+
+//POST api/notes
+
+//ability to delete said notes
